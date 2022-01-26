7d6ce17d0351f952d67f47931f92401d21213b93
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-01-26 23:24:26 +0200

    ✨ feat: new source codes

--- src/utils/contracts.js
+++ src/utils/contracts.js
@@ -1,7 +1,9 @@
-export const REGISTRY_CONTRACT = "83mIdTzof8FeTqgeHgiopzgeOY-5tSJicZGAUEEF07Q";
+export const REGISTRY_CONTRACT = "mHqkTWPghOxHvlGHZDqtB0wlu3SsdTmYMZA1x2hUCno";
 // Previous versions
 // hSKeBf0YhmuDVXpNgMjM664yGMTqX8vqCCU3W2cao3M
+// 83mIdTzof8FeTqgeHgiopzgeOY-5tSJicZGAUEEF07Q
 
-export const TRIBUS_SRC = "YjlgiK_s9ux0tt3tPzff-Y2w28GrRhN5WQGDmNZJQUM";
+export const TRIBUS_SRC = "5Oqqmrz-HlaADbrahUEKHNzsX4XvBV-JydTS68uT0bY";
 // Previous versions
 // EuP-fNaapQC_kzW6slcRvYr31JK8wkSnrPqOPyiMqXc
+// YjlgiK_s9ux0tt3tPzff-Y2w28GrRhN5WQGDmNZJQUM
