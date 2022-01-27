f71181503004223e3c9ea0142c506585787d5d69
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-01-27 13:25:47 +0200

    ✨ feat: CONTRACTS_API SRC

--- src/utils/contracts.js
+++ src/utils/contracts.js
@@ -7,3 +7,5 @@ export const TRIBUS_SRC = "5Oqqmrz-HlaADbrahUEKHNzsX4XvBV-JydTS68uT0bY";
 // Previous versions
 // EuP-fNaapQC_kzW6slcRvYr31JK8wkSnrPqOPyiMqXc
 // YjlgiK_s9ux0tt3tPzff-Y2w28GrRhN5WQGDmNZJQUM
+
+export const CONTRACTS_API = "z1J-k77bSz4IrnreKKLc1ZwBCVTcn7AUc5YQNAm61QU";
