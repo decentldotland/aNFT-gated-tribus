60e7949c9e59dccf39b12fce279fd68e8ab725ad
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-01-27 23:42:09 +0200

    🔨 refactor: update TRIBUS SRC

--- src/utils/contracts.js
+++ src/utils/contracts.js
@@ -3,7 +3,7 @@ export const REGISTRY_CONTRACT = "mHqkTWPghOxHvlGHZDqtB0wlu3SsdTmYMZA1x2hUCno";
 // hSKeBf0YhmuDVXpNgMjM664yGMTqX8vqCCU3W2cao3M
 // 83mIdTzof8FeTqgeHgiopzgeOY-5tSJicZGAUEEF07Q
 
-export const TRIBUS_SRC = "5Oqqmrz-HlaADbrahUEKHNzsX4XvBV-JydTS68uT0bY";
+export const TRIBUS_SRC = "heFS71rLtAIy84ArORo1Ox-4T1FXwTkb6ewJ5zwSwSs";
 // Previous versions
 // EuP-fNaapQC_kzW6slcRvYr31JK8wkSnrPqOPyiMqXc
 // YjlgiK_s9ux0tt3tPzff-Y2w28GrRhN5WQGDmNZJQUM
